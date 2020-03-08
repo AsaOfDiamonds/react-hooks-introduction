8000898de9a8ddf2eb3be142b3aecfc00e2bdd18
Asa Shalom <asa@Asas-iMac.lan>
2020-03-07 18:47:22 -0700

    mostly complete just need to redo notes and minor debug

--- src/hooks/http.js
+++ src/hooks/http.js
@@ -6,7 +6,7 @@
   // call, not nested in a function or a for loop
 import {  useState, useEffect } from "react";
 
-export const useHttp = (url) => {
+export const useHttp = (url, dependencies) => {
     const [ isLoading, setIsLoading] = useState(false);
     // using fetchedData and setFetchedData because we want to use in different components
     // and the data may be different in each
@@ -15,26 +15,18 @@ export const useHttp = (url) => {
 
     // URL wont always be the same so lets set it to a hook
     // fetch('https://swapi.co/api/people')
-    fetch(url)
+    useEffect(() => {
+      setIsLoading(true);
+      console.log('Sending Http request to URL: ' + url);
+      fetch(url)
       .then(response => {
         if (!response.ok) {
           throw new Error('Failed to fetch.');
         }
         return response.json();
       })
-
-      // rename charData to Data
       .then(data => {
-        // const selectedCharacters = charData.results.slice(0, 5);
-        // // setIsLoading false because we are done
-        // setIsLoading(false);
-        // // setloadedChars to what was in this.setState
-        // setLoadedChars(
-        //   selectedCharacters.map((char, index) => ({
-        //       name: char.name,
-        //       id: index + 1
-        //     }))
-        //   );
+        
         setIsLoading(false);
         setFetchedData(data);
       })
@@ -42,10 +34,38 @@ export const useHttp = (url) => {
         console.log(err);
         setIsLoading(false);
       });
+    }, dependencies);
+    // fetch(url)
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error('Failed to fetch.');
+    //     }
+    //     return response.json();
+    //   })
+
+    //   // rename charData to Data
+    //   .then(data => {
+    //     // const selectedCharacters = charData.results.slice(0, 5);
+    //     // // setIsLoading false because we are done
+    //     // setIsLoading(false);
+    //     // // setloadedChars to what was in this.setState
+    //     // setLoadedChars(
+    //     //   selectedCharacters.map((char, index) => ({
+    //     //       name: char.name,
+    //     //       id: index + 1
+    //     //     }))
+    //     //   );
+    //     setIsLoading(false);
+    //     setFetchedData(data);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //     setIsLoading(false);
+    //   });
       // return something see around 49:00
       // in this case an array but it could have been an object or something else
       return [isLoading, fetchedData]
-}
+};
 
 // taken from CharPicker.js to modify
 
